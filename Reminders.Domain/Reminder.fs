df846f752d0164905afc8f4e4bf2bb41a82acaba
ldscavo <logan@scavo.dev>
2022-08-04 17:45:17 -0400

    Add project files.

---
+++ Reminders.Domain/Reminder.fs
@@ -0,0 +1,36 @@
+﻿module Reminders.Domain
+
+open System
+
+type Day =
+    | Sunday
+    | Monday
+    | Tuesday
+    | Wednesday
+    | Thursday
+    | Friday
+    | Saturday
+
+type NthDayOfWeek =
+    { Place: int
+      Day: Day }
+
+type MonthRule =
+    | DateOfMonth of int
+    | NthDayOfWeek of NthDayOfWeek
+
+type Interval =
+    | Day of int
+    | Week of Day list
+    | Month of MonthRule
+    | Year of int
+
+type Recurrance =
+    { Interval: Interval
+      EndsOn: DateTime option }
+
+type Reminder =
+    { Id: Guid
+      Text: string
+      SetFor: DateTime
+      Recurrance: Recurrance option }
